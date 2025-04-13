69e852576e3ed2d8fcb7daffe37aa17b484ad19a
JakeBrowning90 <jacobbrowning90@gmail.com>
2025-04-13 13:10:57 -0400

    Add optimized solution for 'leaderboard' problem

--- week3.js
+++ week3.js
@@ -20,27 +20,60 @@
 // SHERLOCK AND THE VALID STRING
 
 // CLIMBING THE LEADERBOARD
+// Optimized
 function climbingLeaderboard(ranked, player) {
-  let result = [];
-  for (let i = 0; i < player.length; i++) {
-    ranked.push(player[i]);
-    ranked.sort((a, b) => b - a);
-    let position = 0;
-    for (let j = 0; j < ranked.length; j++) {
-      if (ranked[j] !== ranked[j - 1]) {
-        position++;
-      }
-      if (player[i] == ranked[j]) {
-        result.push(position);
-        break;
+  //Declare nested function, binary search
+  const binSearch = (uniqueRanked, score) => {
+    let start = 0;
+    let end = uniqueRanked.length - 1;
+    while (start <= end) {
+      const mid = Math.floor((start + end) / 2);
+      if (uniqueRanked[mid] == score) {
+        return mid + 1;
+      } else if (score > uniqueRanked[mid]) {
+        end = mid - 1;
+      } else {
+        start = mid + 1;
       }
     }
-  }
+    return start + 1;
+  };
+
+  // Use Set to get only unique values from ranked
+  const uniqueRanked = [...new Set(ranked)];
+  // Check
+  let result = [];
+  player.map((score) => {
+    result.push(binSearch(uniqueRanked, score));
+    console.log(uniqueRanked);
+  });
   return result;
 }
+
+// Original solution, unoptimized
+// function altClimbingLeaderboard(ranked, player) {
+//   let result = [];
+//   for (let i = 0; i < player.length; i++) {
+//     ranked.push(player[i]);
+//     ranked.sort((a, b) => b - a);
+//     let position = 0;
+//     for (let j = 0; j < ranked.length; j++) {
+//       if (ranked[j] !== ranked[j - 1]) {
+//         position++;
+//       }
+//       if (player[i] == ranked[j]) {
+//         result.push(position);
+//         break;
+//       }
+//     }
+//   }
+//   return result;
+// }
+
 const ranked = [100, 90, 90, 80];
 const player = [70, 80, 105];
 console.log(climbingLeaderboard(ranked, player));
+// console.log(altClimbingLeaderboard(ranked, player));
 
 // REVERSE A LINKED LIST
 
