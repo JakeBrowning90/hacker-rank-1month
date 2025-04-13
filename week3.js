bfa84013b237c261e0b88c36379c52855eccd7d0
JakeBrowning90 <jacobbrowning90@gmail.com>
2025-04-13 12:21:52 -0400

    Add unoptimized solution for 'Climbing the Leaderboard'

--- week3.js
+++ week3.js
@@ -20,6 +20,27 @@
 // SHERLOCK AND THE VALID STRING
 
 // CLIMBING THE LEADERBOARD
+function climbingLeaderboard(ranked, player) {
+  let result = [];
+  for (let i = 0; i < player.length; i++) {
+    ranked.push(player[i]);
+    ranked.sort((a, b) => b - a);
+    let position = 0;
+    for (let j = 0; j < ranked.length; j++) {
+      if (ranked[j] !== ranked[j - 1]) {
+        position++;
+      }
+      if (player[i] == ranked[j]) {
+        result.push(position);
+        break;
+      }
+    }
+  }
+  return result;
+}
+const ranked = [100, 90, 90, 80];
+const player = [70, 80, 105];
+console.log(climbingLeaderboard(ranked, player));
 
 // REVERSE A LINKED LIST
 
